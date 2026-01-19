53bad1259823ea68d2bca88df1ee6b2905e5561c
ByGamer01 <davidgilrosa1@gmail.com>
2026-01-19 17:24:53 +0100

    Grid Layout 2x2

---
+++ unitt6/entregaSwing/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+import javax.swing.*;
+
+import java.awt.*;
+
+public class Main {
+    public static void main() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,2));
+        
+    }
+}
