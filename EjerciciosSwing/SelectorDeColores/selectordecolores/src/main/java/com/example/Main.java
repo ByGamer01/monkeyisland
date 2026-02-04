6128b0109273845559843e1c18a3cb70ef7fbb68
ByGamer01 <davidgilrosa1@gmail.com>
2026-02-04 20:32:25 +0100

    zs

--- EjerciciosSwing/SelectorDeColores/selectordecolores/src/main/java/com/example/Main.java
+++ EjerciciosSwing/SelectorDeColores/selectordecolores/src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame ventana = new JFrame();
+        ventana.setSize(200, 200);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JPanel panelBotones = new JPanel();
+        JButton botonAzul = new JButton();
+        JButton botonRojo = new JButton();
+        JButton botonVerde = new JButton();
+
+        JPanel panelColor = new JPanel();
+        panelBotones.setBackground(Color.WHITE);
+
+
+        ventana.add(panelBotones, BorderLayout.NORTH);
+        ventana.add(panelColor, BorderLayout.CENTER);
+        
+        ventana.setVisible(true);
     }
 }
